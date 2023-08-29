74e3924da93da0d86546f594a5115e26fc446273
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 14:51:34 +0300

    Add files via upload

---
+++ 011_if_comparisons.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+
+  <?php
+      function getMax($num1, $num2, $num3){
+          if($num1 >= $num2 && $num1 >= $num3){
+            return $num1;
+          } elseif($num2 >= $num1 && $num2 >= $num3) {
+            return $num2;
+          } else {
+            return $num3;
+          }
+      }
+      echo getMax(900, 85 , 105);
+
+  ?>
+  </body>
+</html>
